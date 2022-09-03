f12c3f909a410b662c385e1590126e4d2eb791c3
khanhnguyen <khanhnguyen9329@gmail.com>
2022-09-04 00:19:03 +0700

    time

---
+++ src/System/Time.java
@@ -0,0 +1,10 @@
+package System;
+
+import java.util.Date;
+
+public class Time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Date now" + now);
+    }
+}
