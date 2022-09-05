752c2e08732e869063a6e36347cf18f1398a47d3
khanhnguyen <khanhnguyen9329@gmail.com>
2022-09-05 17:26:46 +0700

    Hiển thị lời chào

---
+++ src/baitap/hienthiloichao.java
@@ -0,0 +1,14 @@
+package baitap;
+
+import java.util.Scanner;
+
+public class hienthiloichao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What your name :");
+        String name;
+        name = sc.nextLine();
+        System.out.println("Hello :" + name);
+
+    }
+}
