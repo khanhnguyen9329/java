622d68dc5dff249e31823ee3b01f8d6a37aacb8f
khanhnguyen <khanhnguyen9329@gmail.com>
2022-09-09 16:54:04 +0700

    Lớp Student

---
+++ src/OOP/Student.java
@@ -0,0 +1,20 @@
+package OOP;
+
+public class Student {
+  private   String name;
+   private String classes;
+    public Student(String name, String classes){
+        this.name = name;
+        this.classes = classes;
+
+    }
+    public Student(){
+
+    }
+    protected void setName(String name){
+
+    }
+    public void setClasses(String classes){
+
+    }
+}
---
+++ src/OOP/mainStuden.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class mainStuden {
+    public static void main(String[] args) {
+        Student sv = new Student();
+ sv.setName("Khanh");
+ sv.setClasses("C0722G1");
+    }
+}
